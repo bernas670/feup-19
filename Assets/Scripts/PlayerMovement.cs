901449472a11ec61149d0c583d39cad5bad7748b
Bernardo Santos <bernardosantos670@gmail.com>
2021-02-20 19:05:25 +0000

    Implemented simple jumping mechanic

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public Transform feet;
+    public LayerMask groundLayer;
+
+
+    public float jumpForce = 5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+
+
+    private void Jump()
+    {
+        if (IsOnGround())
+        {
+            // Vector2 movement = new Vector2(rb.velocity.x, jumpForce);
+            // rb.velocity = movement;
+
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+
+    }
+
+    private bool IsOnGround()
+    {
+        Collider2D groundCheck = Physics2D.OverlapCircle(feet.position, 0.1f, groundLayer);
+
+        if (groundCheck != null)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
