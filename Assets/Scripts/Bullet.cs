6fe2ff999cdf3602874a60b0cfd3db817f4bcf03
Bernardo Santos <bernardosantos670@gmail.com>
2021-02-23 16:49:28 +0000

    Implemented simple shooting

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float speed = 20f;
+    public Rigidbody2D rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D hitInfo) {
+        
+        Debug.Log(hitInfo.name);
+
+        Destroy(gameObject);
+    }
+
+    private void OnBecameInvisible() {
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")) {
+            Shoot();
+        }
+    }
+
+    void Shoot() {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+}
