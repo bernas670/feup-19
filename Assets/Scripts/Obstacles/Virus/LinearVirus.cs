1cbd1f854146eba83a294b210570ee7c5cbbc958
Bernardo Santos <bernardosantos670@gmail.com>
2021-03-06 18:28:46 +0000

    Started implementing virus obstacle

---
+++ Assets/Scripts/Obstacles/Virus/LinearVirus.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class LinearVirus : Virus
+{
+
+    public Vector3[] spots;
+    private int nextSpot = 1;
+
+
+    void Start()
+    {
+        transform.position = spots[nextSpot];
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, spots[nextSpot], speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, spots[nextSpot]) <= 0.1) {
+            nextSpot++;
+
+            if (nextSpot >= spots.Length) {
+                nextSpot = 0;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Obstacles/Virus/Virus.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Virus : MonoBehaviour
+{
+    public float speed = 10f;
+
+}
