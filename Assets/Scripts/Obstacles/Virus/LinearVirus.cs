0ea05bda5e773aa055e1749216f31de8a6667136
Cajo Albuquerque <carlosjo.da@gmail.com>
2021-03-06 20:09:01 +0000

    Add patrolling virus prefab

--- Assets/Scripts/Obstacles/Virus/LinearVirus.cs
+++ Assets/Scripts/Obstacles/Virus/LinearVirus.cs
@@ -2,26 +2,49 @@
 
 public class LinearVirus : Virus
 {
-
-    public Vector3[] spots;
+    private Transform[] spots;
+    private Rigidbody2D rb;
+    private int currentSpot = 0;
     private int nextSpot = 1;
-
 
     void Start()
     {
-        transform.position = spots[nextSpot];
+        Transform[] tempSpots = transform.GetChild(1).GetComponentsInChildren<Transform>();
+        int counter = 0;
+        spots = new Transform[tempSpots.Length - 1];
+        
+        foreach (var spot in tempSpots)
+        {
+            if(spot.gameObject.name.CompareTo("Path") == 0)
+                continue;
+
+            spots.SetValue(spot, counter);
+            counter++;
+        }
+
+        rb = transform.GetComponentInChildren<Rigidbody2D>();
+        rb.position = spots[currentSpot].position;
+        this.SetVelocity();
     }
 
     void FixedUpdate()
     {
-        transform.position = Vector3.MoveTowards(transform.position, spots[nextSpot], speed * Time.deltaTime);
-
-        if (Vector3.Distance(transform.position, spots[nextSpot]) <= 0.1) {
-            nextSpot++;
-
-            if (nextSpot >= spots.Length) {
-                nextSpot = 0;
-            }
+        if (Vector3.Distance(rb.position, spots[nextSpot].position) <= 0.3f) {
+            this.UpdateSpots();
+            this.SetVelocity();
         }
+    }
+
+    private void SetVelocity()
+    {
+        Vector3 dir = (spots[nextSpot].position - new Vector3(rb.position.x, rb.position.y, 0)).normalized;
+        rb.velocity = dir * speed;
+    }
+
+    private void UpdateSpots()
+    {
+        // Works if cyclic (1->2->3->1->2->3->1 ...)
+        currentSpot = (currentSpot + 1) % spots.Length;
+        nextSpot = (nextSpot + 1) % spots.Length;
     }
 }
