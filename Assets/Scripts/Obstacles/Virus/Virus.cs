45c3ab48b4a8186b3a686d3752074bad6f933308
Bernardo Santos <bernardosantos670@gmail.com>
2021-03-07 15:01:28 +0000

    Implemented virus collisions

--- Assets/Scripts/Obstacles/Virus/Virus.cs
+++ Assets/Scripts/Obstacles/Virus/Virus.cs
@@ -6,4 +6,12 @@ public class Virus : MonoBehaviour
 {
     public float speed = 10f;
 
+
+    public void OnTriggerEnter2DChild(Collider2D other) {
+        PlayerHealth player = other.GetComponent<PlayerHealth>();
+
+        if (player) {
+            player.TakeDamage();
+        }
+    }
 }
---
+++ Assets/Scripts/Obstacles/Virus/VirusCollider.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class VirusCollider : MonoBehaviour
+{
+
+    private Virus parentScript;
+
+    private void Awake() {
+        parentScript = GetComponentInParent<Virus>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        parentScript.OnTriggerEnter2DChild(other);
+    }
+}
