b730e0896f19100a997af6d775c85eb3e19447ee
Cajo Albuquerque <carlosjo.da@gmail.com>
2021-03-09 13:49:11 +0000

    Add Power Up spawner; Add FireRatePW animation

---
+++ Assets/Scripts/PowerUps/PowerUpSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PowerUpSpawner : MonoBehaviour
+{
+    public float spawnInterval = 4; //in s
+    public float xPos = 9;
+    public float yPos = -1.5f;
+
+    public GameObject[] powerUpPrefabs;
+    public LevelGenerator generator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(PowerUpSpawn());
+    }
+
+    IEnumerator PowerUpSpawn()
+    {
+        while (true)
+        {
+            int chosenIndex = Random.Range(0, powerUpPrefabs.Length);
+
+            //TODO: if index == Length then Generate Nothing??
+            InstantiatePowerUp(powerUpPrefabs[chosenIndex]);
+
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+
+    void InstantiatePowerUp(GameObject powerUpPrefab)
+    {
+        GameObject powerUp = Instantiate(powerUpPrefab, new Vector3(xPos, yPos, 0), Quaternion.identity);
+        powerUp.transform.parent = generator.GetStage(1);
+    }
+}
