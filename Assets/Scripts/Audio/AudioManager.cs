799d2cfc714cf6765339718780b01dceff1016d3
Cajo Albuquerque <carlosjo.da@gmail.com>
2021-03-14 16:53:14 +0000

    Add singleton AudioManager and Sound; Add main theme file

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,59 @@
+﻿/*
+*   This script was made following a tutorial video by Brackeys
+*   "Introduction to AUDIO in Unity", Brackeys, 31/05/2017
+*   https://www.youtube.com/watch?v=6OT43pvUyfY
+*/
+using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+    public static AudioManager instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            // When a new AudioManagar is instantiated, if another
+            // one already exists then we destroy the new one.
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    void Start()
+    {
+        Play("Theme");
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found.");
+            return;
+        }
+
+        s.source.Play();
+    }
+}
---
+++ Assets/Scripts/Audio/Sound.cs
@@ -0,0 +1,24 @@
+﻿/*
+*   This script was made following a tutorial video by Brackeys
+*   "Introduction to AUDIO in Unity", Brackeys, 31/05/2017
+*   https://www.youtube.com/watch?v=6OT43pvUyfY
+*/
+using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume;
+    [Range(0.1f, 3f)]
+    public float pitch;
+
+    public bool loop;
+
+    [HideInInspector]
+    public AudioSource source;
+}
