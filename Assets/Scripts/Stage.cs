a4cdb0979238897caab5f41ef6a7630b8204eaaa
Bernardo Santos <bernardosantos670@gmail.com>
2021-03-17 22:35:41 +0000

    Implemented random backgrounds

---
+++ Assets/Scripts/Stage.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Stage : MonoBehaviour
+{
+
+    public GameObject[] backgrounds;
+    private Vector3 offset = new Vector3(6, 0, 0);
+
+    private void Awake() {
+        int i = Random.Range(0, backgrounds.Length);
+
+        GameObject background = Instantiate(backgrounds[i], transform.position + offset, Quaternion.identity);
+        background.transform.SetParent(transform);
+    }
+}
