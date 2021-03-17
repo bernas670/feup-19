d0b776a4e411c8f3265ec33ac2f55af59f605d55
Cajo Albuquerque <carlosjo.da@gmail.com>
2021-03-17 16:55:59 +0000

    Add enemy-player collision; Remove unused imports

--- Assets/Scripts/Obstacles/Virus/Virus.cs
+++ Assets/Scripts/Obstacles/Virus/Virus.cs
@@ -1,11 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Virus : MonoBehaviour
 {
     public float speed = 10f;
-
 
     public void OnTriggerEnter2DChild(Collider2D other) {
         PlayerHealth player = other.GetComponent<PlayerHealth>();
--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,7 +1,5 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
