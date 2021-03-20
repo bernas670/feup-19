d88b166e0e1138e7a083ce34cea0deba4b138287
Cajo Albuquerque <carlosjo.da@gmail.com>
2021-03-20 23:08:51 +0000

    Add homemade sounds :3

--- Assets/Scripts/UI/GameOverScreen.cs
+++ Assets/Scripts/UI/GameOverScreen.cs
@@ -1,19 +1,17 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System;
 
 public class GameOverScreen : MonoBehaviour
 {
-
     public Text scoreText;
+    public Text timerText;
 
-    private void Awake() {
-        gameObject.SetActive(false);
-    }
-
-    public void Setup(int score) {
+    public void Setup(int score, float time) {
         gameObject.SetActive(true);
         scoreText.text = string.Format("Score: {0}", score);
+        timerText.text = "Time: " + TimeSpan.FromSeconds(time).ToString(@"mm\:ss\.f");
     }
 
     public void RestartButton() {
