bb54fcf29caba86b4656e429b038e1d49236cb4f
Cajo Albuquerque <carlosjo.da@gmail.com>
2021-03-21 00:02:48 +0000

    Add input delay to GameOver screen

--- Assets/Scripts/UI/GameOverScreen.cs
+++ Assets/Scripts/UI/GameOverScreen.cs
@@ -2,23 +2,39 @@
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System;
+using System.Collections;
 
 public class GameOverScreen : MonoBehaviour
 {
     public Text scoreText;
     public Text timerText;
+    public int inputDelay = 1;
+    private bool inputEnabled = false;
 
-    public void Setup(int score, float time) {
+    public void Setup(int score, float time)
+    {
         gameObject.SetActive(true);
         scoreText.text = string.Format("Score: {0}", score);
         timerText.text = "Time: " + TimeSpan.FromSeconds(time).ToString(@"mm\:ss\.f");
+        
+        StartCoroutine(EnableInput());
     }
 
-    public void RestartButton() {
-        SceneManager.LoadScene("Game");
+    private IEnumerator EnableInput()
+    {
+        yield return new WaitForSecondsRealtime(inputDelay);
+        inputEnabled = true;
     }
 
-    public void ReturnToMenuButton() {
-        SceneManager.LoadScene("MainMenu");
+    public void RestartButton()
+    {
+        if (inputEnabled)
+            SceneManager.LoadScene("Game");
+    }
+
+    public void ReturnToMenuButton()
+    {
+        if (inputEnabled)
+            SceneManager.LoadScene("MainMenu");
     }
 }
